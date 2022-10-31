65a5d7b61662bdef2665cdddd48d40e18e98d905
bb <oleh.kvsk@gmail.com>
2022-10-31 14:48:03 +0200

    greetingscreen changes

--- modules/home/GreetingScreen/GreetingScreen.tsx
+++ modules/home/GreetingScreen/GreetingScreen.tsx
@@ -7,6 +7,7 @@ import { Autoplay, Pagination } from "swiper"
 import classNames from "classnames"
 import LinkWithLine from "components/LinkWithLine"
 import Socials from "./Socials"
+import Link from "next/link"
 
 const slides = new Array(4).fill({
   primaryText: "we create impressive architecture and modern interiors",
@@ -17,55 +18,41 @@ const slides = new Array(4).fill({
 function GreetingScreen() {
   return (
     <div className={classNames(baseStyles.wrapper, styles.wrapper)}>
-      <a className={styles.allProjectsLink}>all projects</a>
-      <a className={styles.languageChange}>en</a>
-      <div className={styles.swiperWrapper}>
-        <Swiper
-          autoplay
-          modules={[Pagination, Autoplay]}
-          pagination={{
-            el: `.${styles.swiperPaginationWrapper}`,
-            clickable: true,
-            horizontalClass: styles.swiperPagination,
-            bulletClass: styles.bullet,
-            bulletActiveClass: styles.bulletActive,
-            renderBullet: (index, className) => {
-              return `<span class=${className}>${index + 1}</span>`
-            },
-          }}
-          slidesPerView="auto"
-          spaceBetween={100}
-          loop={true}
-          speed={1000}
-          className={styles.swiper}
-        >
-          {slides.map(({ primaryText, secondaryText }, index) => (
-            <SwiperSlide className={styles.slide} key={index}>
-              {({ isActive }) => (
-                <>
-                  <span
-                    className={classNames(
-                      styles.slideTextPrimary,
-                      isActive && styles.slideTextPrimaryActive
-                    )}
-                  >
-                    {primaryText}
-                  </span>
-                  <span
-                    className={classNames(
-                      styles.slideTextSecondary,
-                      isActive && styles.slideTextSecondaryActive
-                    )}
-                  >
-                    {secondaryText}
-                  </span>
-                </>
-              )}
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      <Link href="#" className={styles.allProjectsLink}>
+        all projects
+      </Link>
+      <Swiper
+        // autoplay
+        modules={[Pagination, Autoplay]}
+        pagination={{
+          el: `.${styles.swiperPaginationWrapper}`,
+          clickable: true,
+          horizontalClass: styles.swiperPagination,
+          bulletClass: styles.bullet,
+          bulletActiveClass: styles.bulletActive,
+          renderBullet: (index, className) => {
+            return `<span class=${className}>${index + 1}</span>`
+          },
+        }}
+        slidesPerView="auto"
+        spaceBetween={100}
+        loop={true}
+        speed={1000}
+        className={styles.swiper}
+      >
+        {slides.map(({ primaryText, secondaryText }, index) => (
+          <SwiperSlide className={styles.slide} key={index}>
+            <h2>{primaryText}</h2>
+            <p className={classNames(styles.slideTextSecondary)}>
+              {secondaryText}
+            </p>
+          </SwiperSlide>
+        ))}
+      </Swiper>
       <LinkWithLine wrapperClassName={styles.link}>view more</LinkWithLine>
+      <Link href="#" className={styles.languageChange}>
+        en
+      </Link>
       <div className={styles.swiperPaginationWrapper} />
       <Socials className={styles.socials} />
       <Image
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import type { NextPage } from "next"
 import GreetingScreen from "modules/home/GreetingScreen"
 import AboutCompanyScreen from "modules/home/AboutCompanyScreen"
 import PapersScreen from "modules/home/PapersScreen"
 import ProjectsScreen from "modules/home/ProjectsScreen"
 import Footer from "Layout/Footer"
 
-const Home: NextPage = () => {
+function Home() {
   return (
     <>
       <GreetingScreen />
