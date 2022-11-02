37fd3142a6b10b0009f68a08c1d37b5535e08b89
bb <oleh.kvsk@gmail.com>
2022-11-02 13:29:06 +0200

    inst pics added

--- Layout/Footer/Socials/Swiper/SocialsSwiper.tsx
+++ Layout/Footer/Socials/Swiper/SocialsSwiper.tsx
@@ -1,18 +1,16 @@
 import { Swiper, SwiperProps, SwiperSlide } from "swiper/react"
 import Image from "next/image"
-import image from "assets/dummyPics/instagramPhoto.png"
+import image1 from "assets/dummyPics/instagramPhotos/1.jpg"
+import image2 from "assets/dummyPics/instagramPhotos/2.jpg"
+import image3 from "assets/dummyPics/instagramPhotos/3.jpg"
+import image4 from "assets/dummyPics/instagramPhotos/4.jpg"
 import styles from "./SocialsSwiper.module.css"
 import { Autoplay } from "swiper"
-import { useRef, useState } from "react"
 import classNames from "classnames"
 
-const slides = new Array(10).fill(10)
+const images = [image1, image2, image3, image4]
 
 function SocialsSwiper({ className, ...props }: SwiperProps) {
-  const ref = useRef<null | any>(null)
-
-  const [swiper, setSwiper] = useState<any>(null)
-
   return (
     <Swiper
       modules={[Autoplay]}
@@ -21,12 +19,10 @@ function SocialsSwiper({ className, ...props }: SwiperProps) {
       className={classNames(styles.swiper, className)}
       loop
       speed={10000}
-      onSwiper={(swiper) => setSwiper(swiper)}
       autoplay={{ delay: 1 }}
-      onClick={() => swiper.slideNext()}
       {...props}
     >
-      {slides.map((slide, index) => (
+      {images.map((image, index) => (
         <SwiperSlide key={index} className={styles.slide}>
           <Image src={image} alt="" />
         </SwiperSlide>
