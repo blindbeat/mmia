659dc2669d24e2c8d351d31c760fe720f9ef2280
bb <oleh.kvsk@gmail.com>
2022-11-20 22:45:49 +0200

    cta link fix

--- Layout/Header/CtaLink/CtaLink.tsx
+++ Layout/Header/CtaLink/CtaLink.tsx
@@ -1,31 +1,56 @@
 import Link, { LinkProps } from "next/link"
 import styles from "./CtaLink.module.css"
-import CtaLine from "./assets/ctaLine.svg"
 import classNames from "classnames"
-import { ComponentPropsWithoutRef, useState } from "react"
+import { ComponentPropsWithoutRef, useEffect, useRef, useState } from "react"
+import { calcPathLength } from "misc/utils"
 
 function CtaLink({
   className,
   ...rest
 }: ComponentPropsWithoutRef<"a"> & LinkProps) {
-  const [animationPlayState, setAnimationPlayState] =
-    useState<AnimationPlayState>("paused")
+  const [isAnimationPlaying, setIsAnimationPlaying] = useState(false)
+
+  const pathRef = useRef<SVGPathElement | null>(null)
+  const [lineLength, setLineLength] = useState(0)
+  const [iteration, setIteration] = useState(0)
+
+  useEffect(() => {
+    const path = pathRef.current
+    if (!path) return
+    setLineLength(calcPathLength(path))
+  }, [])
 
   return (
     <Link
       className={classNames(styles.content, className)}
-      onMouseEnter={() => setAnimationPlayState("running")}
-      onAnimationIteration={() => setAnimationPlayState("paused")}
+      onMouseEnter={() => {
+        if (isAnimationPlaying) return
+        setIteration(iteration + 1)
+        setIsAnimationPlaying(true)
+      }}
+      onTransitionEnd={() => setIsAnimationPlaying(false)}
       {...rest}
     >
       <span>
         drop request
-        <CtaLine
-          className={classNames(
-            styles.line,
-            animationPlayState === "paused" ? styles.paused : styles.running
-          )}
-        />
+        <svg
+          viewBox="0 0 136 4"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={styles.svg}
+        >
+          <path
+            style={{
+              strokeDasharray: lineLength,
+              strokeDashoffset: iteration * lineLength * -2,
+            }}
+            className={styles.path}
+            ref={pathRef}
+            opacity="0.2"
+            d="M1 1C38.7206 5.49999 87.0294 1.00001 136 1"
+            vectorEffect="non-scaling-stroke"
+          />
+        </svg>
       </span>
     </Link>
   )
