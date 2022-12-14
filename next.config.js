3538f8006b7ec74551691878ad7a75b5568418dc
bb <oleh.kvsk@gmail.com>
2022-12-14 14:19:29 +0200

    Avif support

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
